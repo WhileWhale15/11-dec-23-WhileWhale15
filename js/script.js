ae0bca111545cf83999319e877e68b5ecd593c60
Affifah Aulia Zahra <113563660+WhileWhale15@users.noreply.github.com>
2023-12-20 20:49:26 +0700

    updated styling

--- js/script.js
+++ js/script.js
@@ -9,3 +9,16 @@ document.getElementById("submit").addEventListener("click", function () {
   const nama = nameInput.value;
   document.getElementById("name").innerHTML = nama;
 });
+
+// Slider
+let currentIndex = 0;
+const images = document.querySelectorAll(".slider-content");
+const totalImages = images.length(3);
+
+function changeImage() {
+  images[currentIndex].style.display = "none";
+  currentIndex = (currentIndex + 1) % totalImages;
+  images[currentIndex].style.display = "block";
+}
+
+setInterval(changeImage, 2000);
