ad2560ed66dd1f660f3a05b57380634fc896eaca
Affifah Aulia Zahra <113563660+WhileWhale15@users.noreply.github.com>
2023-12-21 14:23:33 +0700

    adding new feature for product showcase

--- js/script.js
+++ js/script.js
@@ -23,9 +23,41 @@ function changeImage() {
 
 setInterval(changeImage, 2000);
 
+// Navbar
 function toggleNav() {
   var menuToggle = document.querySelector(".toggle");
   var navigation = document.querySelector(".nav-bar");
   menuToggle.classList.toggle("active");
   navigation.classList.toggle("active");
+}
+
+// Thumbnail
+function imgSlider(anything, switchClass) {
+  document.querySelector(switchClass).src = anything;
+}
+
+function changeCategory(newText, displayNameId) {
+  document.getElementById(displayNameId).textContent = newText;
+}
+
+function changeText(newText, displayTextId) {
+  document.getElementById(displayTextId).textContent = newText;
+}
+
+function set1() {
+  imgSlider("assets/Cherry_Red.png", ".switch");
+  changeCategory("Cherry MX Red", "displayName");
+  changeText("Linear", "displayText");
+}
+
+function set2() {
+  imgSlider("assets/Gateron_Red.png", ".switch1");
+  changeCategory("Gateron Red", "displayName1");
+  changeText("Linear", "displayText1");
+}
+
+function set3() {
+  imgSlider("assets/Kailh_BOX_Red.png", ".switch2");
+  changeCategory("Kailh BOX Red", "displayName2");
+  changeText("Linear");
 }
